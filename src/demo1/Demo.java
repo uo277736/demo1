529d42a3c3a84231af1be85e2830d7136a6d3005
user-pc <user-pc@Samu-Serrano>
2022-02-17 10:02:00 +0100

    metodo main

--- src/demo1/Demo.java
+++ src/demo1/Demo.java
@@ -1,5 +1,12 @@
 package demo1;
 
 public class Demo {
+	
+	public static void main(String [] args) {
+		System.out.println("línea 1");
+		System.out.println("línea 1");
+		System.out.println("línea 1");
+		
+	}
 
 }
